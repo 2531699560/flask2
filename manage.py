4d08f63f83497e2cca8f2eb7a7bc6b89491767d9
lijie <18308465146@163.com>
2019-07-17 11:16:56 +0800

    ptoject one

--- manage.py
+++ manage.py
@@ -1,10 +1,13 @@
 from flask_script import Manager
 
-from app.main import create_app
+from app.config import create_app
 
 app = create_app()
 app.debug = True
 manage = Manager(app=app)
 
 if __name__ == '__main__':
-    manage.run()
+    # 修改启动的ip和端口、debug模式
+    app.run(host='192.168.1.156',port=8080,debug=True)
+    # python manage.py runserver -h 0.0.0.0 -p 8080 -d
+    # manage.run()
